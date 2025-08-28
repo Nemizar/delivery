a17551b7b8927d2a72ec9e8ede1ecff31bc422d3
KuzminAV <aleksandr.kuzmin@vseinstrumenti.ru>
2025-08-28 21:05:15 +0300

    module 2

---
+++ internal/core/domain/kernel/location.go
@@ -0,0 +1,78 @@
+package kernel
+
+import (
+	"delivery/internal/pkg/errs"
+	"fmt"
+	"math/rand/v2"
+)
+
+type Location struct {
+	x int
+	y int
+}
+
+const (
+	minXCoordinate = 1
+	maxXCoordinate = 10
+	minYCoordinate = 1
+	maxYCoordinate = 10
+)
+
+func NewLocation(x, y int) (Location, error) {
+	if x > maxXCoordinate || x < minXCoordinate {
+		return Location{}, errs.NewValueIsInvalidError("x")
+	}
+
+	if y > maxYCoordinate || y < minYCoordinate {
+		return Location{}, errs.NewValueIsInvalidError("y")
+	}
+
+	return Location{
+		x: x,
+		y: y,
+	}, nil
+}
+
+func (l Location) X() int {
+	return l.x
+}
+
+func (l Location) Y() int {
+	return l.y
+}
+
+func (l Location) Equals(other Location) bool {
+	return l == other
+}
+
+func (l Location) DistanceTo(other Location) int {
+	return abs(l.x-other.x) + abs(l.y-other.y)
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func RandomLocation() Location {
+	loc, err := NewLocation(
+		randomCoordinate(minXCoordinate, maxXCoordinate),
+		randomCoordinate(minYCoordinate, maxYCoordinate),
+	)
+
+	if err != nil {
+		panic(fmt.Errorf("incorrect random location: %w", err))
+	}
+
+	return loc
+}
+
+func randomCoordinate(min, max int) int {
+	return rand.IntN(max-min+1) + min
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf("(%d,%d)", l.x, l.y)
+}
